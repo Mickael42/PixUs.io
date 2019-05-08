072607301dc95819d9f74df9ad3549bca5de7466
Mickael42 <mickael.wala@gmail.com>
2019-05-08 12:58:09 +0200

    update bdd

--- htdocs/PixUs/bdd/loginBdd.php
+++ htdocs/PixUs/bdd/loginBdd.php
@@ -2,7 +2,7 @@
 // connexion à la base de données PixUs
 try
 {
-    $bdd = new PDO('mysql:host=192.168.1.15;dbname=PixUs;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=127.0.0.1;dbname=PixUs;charset=utf8', 'root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (Exception $e)
